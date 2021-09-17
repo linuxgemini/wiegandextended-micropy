d430fea76fb875e4cbefa4bfaec1f1643c8b0f8e
İlteriş Yağıztegin Eroğlu <ilteris@asenkron.com.tr>
2021-09-17 22:55:01 +0300

    improve screen init

--- src/lib/screen.py
+++ src/lib/screen.py
@@ -1,8 +1,16 @@
-from machine import SoftI2C
+from machine import SoftI2C, I2C, Pin
 from lcd.machine_i2c_lcd import I2cLcd
 
-def init_screen(pin_sda, pin_scl):
-    i2c = SoftI2C(sda=pin_sda, scl=pin_scl)
+def init_screen(pin_sda, pin_scl, conv_to_pin_obj=False, use_hw_i2c=False, hw_i2c_slot=0):
+    if conv_to_pin_obj:
+        pin_sda = Pin(pin_sda)
+        pin_scl = Pin(pin_scl)
+
+    if use_hw_i2c:
+        i2c = I2C(hw_i2c_slot, sda=pin_sda, scl=pin_scl)
+    else:
+        i2c = SoftI2C(sda=pin_sda, scl=pin_scl)
+
     addr = i2c.scan()[0]
 
     return I2cLcd(i2c, addr, 2, 16)
